a7d178815218b11dd2a88c338dd92df458a270b4
habibabdo <65847862+habibabdo@users.noreply.github.com>
2020-06-15 11:58:45 +0300

    Create math.py

---
+++ math.py
@@ -0,0 +1,13 @@
+import math
+print('SQRT OF 5 = ' +str(math.sqrt(25)))
+print('SQRT OF 5 = ' +str(math.sqrt(15)))
+x=math.sqrt(25)
+print(x)
+print('math.floor  of 2.6 IS    ' + str(math.floor(2.6)))
+print('math.ceil   of 2.6 IS    ' + str(math.ceil(2.6)))
+print('POW:')
+print(3**2)
+print (math.pow(3,2))
+import math as m
+print (m.pow(3,2))
+
