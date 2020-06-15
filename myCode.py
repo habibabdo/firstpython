736680c731b1110054f38387502a07eeeb4eefbe
habibabdo <65847862+habibabdo@users.noreply.github.com>
2020-06-15 11:59:13 +0300

    Create myCode.py

---
+++ myCode.py
@@ -0,0 +1,11 @@
+import sys
+
+#x=int(input('Enter Number 1'))
+#y=int(input('Enter Number 2'))
+x=int(sys.argv[1])
+y=int(sys.argv[2])
+z=x+y
+print(z)
+
+#result=eval(input('Enter an expretion'))
+#print(result)
