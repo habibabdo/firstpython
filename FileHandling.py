9257a9eebadca31274f89616861e8b615b219e79
habibabdo <65847862+habibabdo@users.noreply.github.com>
2020-06-15 12:12:51 +0300

    Create FileHandling.py

---
+++ FileHandling.py
@@ -0,0 +1,22 @@
+"""
+assadadad
+ddsdsdsdsds
+"""
+
+
+f = open('mydata','r')
+
+
+#print(f.read())
+#print(f.readline())
+f1=open('abc','r')
+#f1.write('Some Thing')
+#f1.write('  Laptop')
+#f1.write('  Mobile')
+
+for data in f1:
+    print((data))
+f3=open('DODO.gif','rb')
+f4=open('mypic.jpg','wb')
+for i in f3:
+    f4.write(i)
