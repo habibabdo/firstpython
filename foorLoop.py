2b45b2b0ccfe51d72fdf3d9e3e7e41ccb65ac86a
habibabdo <65847862+habibabdo@users.noreply.github.com>
2020-06-15 11:57:27 +0300

    Create foorLoop.py

---
+++ foorLoop.py
@@ -0,0 +1,46 @@
+for i in range(10,20):
+   if(i%3) != 0:
+    print(i)
+
+j=0
+print('\n')
+for j in range(20,10,-1):
+        print(j)
+
+av=5
+x=int(input('How Many  XXXXXX you want'))
+i=1
+while i <= x:
+
+    if i > av:
+        print('Out of range')
+        break
+    print(str(i) + ':XXXX')
+    i += 1
+
+print('Bye')
+
+for i in range(1,50) :
+
+    if (i%3)== 0 or (i%5)==0 or (i%7)==0:
+       continue
+
+    print(i)
+
+print ('bye')
+
+for i in range(1,30) :
+
+    if (i % 2) == 0:
+        pass
+    else:
+
+        print(i)
+
+
+print('bye')
+
+
+
+
+
